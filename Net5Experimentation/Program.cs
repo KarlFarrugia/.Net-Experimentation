bb20eb22c85c520224683aec47275648ff773b60
Karl Farrugia <kfarrugia@exigy.com>
2020-04-12 15:05:30 +0200

    Add project files.

---
+++ Net5Experimentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net5Experimentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
